99f9f9c3b43840438b6ce947ace4945f9840132d
bugra <bgrcmldg@gmail.com>
2020-08-11 21:41:16 +0800

    my latest update

--- src/git_project_second/Test.java
+++ src/git_project_second/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("my second git project");
 		System.out.println("update");
 		System.out.println("remote update from github.com");
+		System.out.println("australia");
 	}
 
 }
