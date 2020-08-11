58318cdebd76f6d0570e1493ba2e48bb88e17002
bugra1993 <65898253+bugra1993@users.noreply.github.com>
2020-08-11 21:34:56 +0800

    Update Test.java
    new uptaded in github

--- src/git_project_second/Test.java
+++ src/git_project_second/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("my second git project");
 		System.out.println("update");
+		System.out.println("remote update from github.com");
 	}
 
 }
